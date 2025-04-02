4a0c5fcb58e45421661e53963c79757c38a6fd17
Luizustavo <luizg.ss@outlook.com>
2025-04-02 20:00:10 -0300

    ``` No code changes made. ```

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 app.use(cors()); // Permite todas as origens; ajuste conforme necessário
 
